f986f8a665652af2b2f2fdf5547b4b4c5e2f4b69
Ahmed HMIDI <ahmed.hmidi@prestataires.boulanger.com>
2022-08-03 13:50:49 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello xCoCoders!'));
 
+app.get('/xcocode', (req, res) => res.send('Contact xCoCoders at: +33 7 65 89 82 14'));
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
